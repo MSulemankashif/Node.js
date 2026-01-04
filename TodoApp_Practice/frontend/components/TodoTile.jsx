0ba5206b3bee1a436e14cd0a11d2841cd487eec8
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-01-04 19:06:03 +0500

    update

--- TodoApp_Practice/frontend/components/TodoTile.jsx
+++ TodoApp_Practice/frontend/components/TodoTile.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
+import axios from 'axios'
 
-function TodoTile({todo}) {
+function TodoTile({todo, onDelete, onEdit}) {
+
+  async function handleDelete(){
+    if (!window.confirm("Are you sure you want to delete this todo?")) return;
+
+    try{
+      await axios.delete(`http://localhost:3000/todos/${todo._id}`);
+      onDelete(todo._id);
+    }catch(error){
+      console.error(error);
+      alert("Failed to delete todo");
+    }
+  }
   return (
     <>
         <div className='list-group-item d-flex align-items-center justify-content-between'>
+        <div>
             <h5>{todo.title}</h5>
             <div>{todo.desc}</div>
-        </div>
+
+            </div>
 
         <div className='d-flex gap-3'>
             <button className='btn btn-sm btn-warning'>Edit</button>
-            <button className='btn btn-sm btn-danger'>Delete</button>
+            <button className='btn btn-sm btn-danger' onClick={handleDelete}>Delete</button>
+        </div>
+
         </div>
     </>  
   )
